787be90564c8f02fc28d877e833cb7437858488f
Simon Birchall <sbirchall@Simons-MacBook-Pro.local>
2016-05-13 16:28:23 +0100

    Get clock to update

--- public/clock/clock.directive.js
+++ public/clock/clock.directive.js
@@ -1,17 +1,28 @@
-angular.module('app', [])
-    .directive('clock', function() {
-        var directive = {
-            replace: false,
-            restrict: 'EA',
-            templateUrl: 'clock/clock.html',
-            link: link
-        };
+(function() {
+    angular.module('app', [])
+        .directive('clock', function($timeout) {
+            var directive = {
+                replace: false,
+                restrict: 'EA',
+                templateUrl: 'clock/clock.html',
+                link: link
+            };
 
-        return directive;
+            return directive;
 
-        function link(scope, elem, attrs) {
-            scope.hours = 10;
-            scope.minutes = 08;
-            scope.seconds = 23;
-        }
-    });
+            function link(scope, elem, attrs) {
+                scope.hours = 10;
+                scope.minutes = 08;
+                scope.seconds = 23;
+                
+                var tick = function() {
+                    var now = moment();
+                    scope.hours = now.format('hh');
+                    scope.minutes = now.format('mm');
+                    scope.seconds = now.format('ss');
+                    $timeout(tick, 1000);
+                };
+                $timeout(tick, 1000);
+            }
+        });
+})();
